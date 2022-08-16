d89e0e6c7060b8cfa78e4d29a128beacc5bbb2b6
MaraOD555 <ruffbc@hotmail.com>
2022-08-16 12:42:41 +0300

    Мастер файл

---
+++ HWLibraries/src/main/java/pro/sky/HWLibraries/HwLibrariesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.HWLibraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwLibrariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwLibrariesApplication.class, args);
+	}
+
+}
---
+++ HWLibraries/src/test/java/pro/sky/HWLibraries/HwLibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.HWLibraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwLibrariesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
